e7fd63c100880abc59f53306a11da7a13a672fa2
adriel <adrielhenrydb@gmail.com>
2026-04-21 16:06:52 -0300

    feat: cria a interface com os metodos pos , get, put, delete

--- lib/app/core/interfaces/i_network_client.dart
+++ lib/app/core/interfaces/i_network_client.dart
@@ -1,3 +1,14 @@
+import 'package:q_sync/app/core/network/result.dart';
+
 abstract class INetworkClient {
-  
+  Future<Result<T>> get<T>(
+    String? path, {
+    Map<String, dynamic>? queryParameters,
+  });
+
+  Future<Result<T>> post<T>(String path, {Object? body});
+
+  Future<Result<T>> put<T>(String path, {Object? body});
+
+  Future<Result<T>> delete<T>(String path);
 }
--- lib/app/core/network/result.dart
+++ lib/app/core/network/result.dart
@@ -2,21 +2,21 @@ sealed class Result<T> {}
 
 class Success<T> extends Result<T> {
   final int? statusCode;
-  final Result? result;
+  final T data;
 
-  Success({this.result, this.statusCode});
+  Success({required this.data, this.statusCode});
 }
 
 class Failure<T> extends Result<T> {
-  final Result result;
   final int? statusCode;
   final String errorMessage;
   final dynamic rawBody;
+  final StackTrace? stackTrace;
 
   Failure({
-    this.errorMessage = '',
+    required this.errorMessage,
     this.rawBody,
-    required this.result,
     this.statusCode,
+    this.stackTrace,
   });
 }
