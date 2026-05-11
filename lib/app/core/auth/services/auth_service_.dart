e2589cb9f6d79c6b9de81953626aa147729eaeeb
Adriel Henry Dias Barbosa <adrielhenrydb@gmail.com>
2026-05-11 11:13:59 -0300

    feat: cria o model de usuario integrando na interface

--- lib/app/core/auth/services/auth_service_.dart
+++ lib/app/core/auth/services/auth_service_.dart
@@ -1,10 +1,10 @@
-import 'package:q_sync/app/core/network/api_client.dart';
+import 'package:q_sync/app/core/auth/models/user_model.dart';
 import 'package:q_sync/app/core/network/result.dart';
 
 abstract class IAuthService {
-  Future<Result<void>> login(String email, String password);
-  Future<Result<void>> resetPassword(String email);
-  Future<Result<void>> logout();
+  Future<Result<UserModel>> login(String email, String password);
+  Future<Result<UserModel>> resetPassword(String email);
+  Future<Result<UserModel>> logout();
   Future<String?> getToken();
-  Future<bool>checkAuthStatus();
+  Future<bool> checkAuthStatus();
 }
